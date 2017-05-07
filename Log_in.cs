ed6b6fbd7ed7c052f10acf568ae8e06bc190fd27
Mahmoud Mahanny <mahmoud.mahany2012@gmail.com>
2017-05-07 14:58:50 +0200

    log in by adam :v:

---
+++ Log_in.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace womenDisease
+{
+    public partial class log_in : Form
+    {
+        public log_in()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+        private void log_in_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Log_in.designer.cs
@@ -0,0 +1,57 @@
+﻿namespace womenDisease
+{
+    partial class log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(log_in));
+            this.SuspendLayout();
+            // 
+            // log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1096, 736);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "log_in";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "مستشفي صحة المرأه ";
+            this.Load += new System.EventHandler(this.log_in_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        public System.EventHandler HHome_Load { get; set; }
+    }
+}
